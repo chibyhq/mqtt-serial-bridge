7216f493852c5d14a5179c5b1e01ab91217cda05
bcopy <500789+bcopy@users.noreply.github.com>
2020-07-21 22:20:53 +0200

    Add support to convert topic/message to serial port

--- modules/core/src/main/java/org/github/chibyhq/msb/mqtt/MqttNamingStrategy.java
+++ modules/core/src/main/java/org/github/chibyhq/msb/mqtt/MqttNamingStrategy.java
@@ -1,10 +1,13 @@
 package org.github.chibyhq.msb.mqtt;
 
+import java.util.Optional;
+
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.github.chibyhq.msb.dto.DeviceOutput;
 import org.github.chibyhq.msb.dto.PortInfo;
 
 /**
- * Implement this interface to let the bridge know how to format
+ * Implement this interface to let the bridge know how to translate
  * MQTT topic names. You can for instance use the contents of the 
  * output message to publish to a given topic, or use the port name
  * or the host name, or a combination of all of these.<br>
@@ -14,4 +17,5 @@ import org.github.chibyhq.msb.dto.PortInfo;
 public interface MqttNamingStrategy {
     String getTopicName(DeviceOutput output);
     String getIncomingMqttCommandTopicForPort(PortInfo portInfo);
+    Optional<String> getPortForTopicNameOrMessage(String topic, MqttMessage message);
 }
