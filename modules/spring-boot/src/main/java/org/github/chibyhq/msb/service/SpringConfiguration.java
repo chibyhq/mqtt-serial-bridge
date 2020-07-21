1ab5fca852c95847fc5d449770873ee7729393a0
bcopy <500789+bcopy@users.noreply.github.com>
2020-07-21 09:14:08 +0200

    Add missing Spring beans

--- modules/spring-boot/src/main/java/org/github/chibyhq/msb/service/SpringConfiguration.java
+++ modules/spring-boot/src/main/java/org/github/chibyhq/msb/service/SpringConfiguration.java
@@ -1,16 +1,56 @@
 package org.github.chibyhq.msb.service;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.github.chibyhq.msb.lineprotocol.SimpleLineProtocolFormatter;
+import org.github.chibyhq.msb.mqtt.MqttErrorHandler;
+import org.github.chibyhq.msb.mqtt.MqttErrorLoggingHandler;
 import org.github.chibyhq.msb.mqtt.MqttNamingStrategy;
+import org.github.chibyhq.msb.mqtt.SerialToMqttForwardingListener;
 import org.github.chibyhq.msb.mqtt.homie.HomieNamingStrategy;
+import org.github.chibyhq.msb.serial.SerialMessageFormatter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
+@Slf4j
 public class SpringConfiguration {
 
-	@Bean MqttNamingStrategy getNamingStrategy() {
-		return new HomieNamingStrategy("localhost");
-	}
-	
+    @Bean
+    MqttNamingStrategy mqttNamingStrategy() {
+        return new HomieNamingStrategy("localhost");
+    }
 
+    @Bean
+    @ConditionalOnProperty("msb.mqtt.forward")
+    MqttClient mqttClient(@Value("${msb.mqtt.client.server.uri:tcp://0.0.0.0:1883}") String serverURI,
+            @Value("${msb.mqtt.client.id}") String clientId) throws MqttException {
+        log.debug("Creating MQTT client to URI {}, client ID {}", serverURI, clientId);
+        MqttClient mqttClient = new MqttClient(serverURI, clientId);
+        return mqttClient;
+    }
+
+    @Bean
+    SerialMessageFormatter messageFormatter() {
+        return new SimpleLineProtocolFormatter();
+    }
+
+    @Bean
+    MqttErrorHandler errorHandler() {
+        return new MqttErrorLoggingHandler();
+    }
+
+    @Bean
+    @Primary
+    @ConditionalOnProperty("msb.mqtt.forward")
+    SerialToMqttForwardingListener serialToMqttForwardingListener(MqttNamingStrategy namingStrategy,
+            MqttClient mqttClient, SerialMessageFormatter formatter, MqttErrorHandler mqttErrorHandler) {
+        return new SerialToMqttForwardingListener(namingStrategy, mqttClient, formatter, mqttErrorHandler);
+    }
+    
 }
