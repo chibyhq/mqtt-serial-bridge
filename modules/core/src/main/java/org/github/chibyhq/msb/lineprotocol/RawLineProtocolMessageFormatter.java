70ada7c4893e4de7142e3c4d527b948ece6774f0
bcopy <500789+bcopy@users.noreply.github.com>
2020-07-21 09:36:39 +0200

    Change class visibility

--- modules/core/src/main/java/org/github/chibyhq/msb/lineprotocol/RawLineProtocolMessageFormatter.java
+++ modules/core/src/main/java/org/github/chibyhq/msb/lineprotocol/RawLineProtocolMessageFormatter.java
@@ -4,7 +4,7 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.github.chibyhq.msb.dto.DeviceOutput;
 import org.github.chibyhq.msb.serial.SerialMessageFormatter;
 
-final class RawLineProtocolMessageFormatter implements SerialMessageFormatter {
+public class RawLineProtocolMessageFormatter implements SerialMessageFormatter {
     @Override
     public MqttMessage getMessageForDeviceOutput(DeviceOutput deviceOutput) {
         String message = deviceOutput.getLine()+" "+deviceOutput.getTimestamp();
--- modules/spring-boot/src/main/java/org/github/chibyhq/msb/service/SpringConfiguration.java
+++ modules/spring-boot/src/main/java/org/github/chibyhq/msb/service/SpringConfiguration.java
@@ -2,8 +2,7 @@ package org.github.chibyhq.msb.service;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.github.chibyhq.msb.dto.DeviceOutput;
+import org.github.chibyhq.msb.lineprotocol.RawLineProtocolMessageFormatter;
 import org.github.chibyhq.msb.mqtt.MqttErrorHandler;
 import org.github.chibyhq.msb.mqtt.MqttErrorLoggingHandler;
 import org.github.chibyhq.msb.mqtt.MqttNamingStrategy;
@@ -38,13 +37,7 @@ public class SpringConfiguration {
 
     @Bean
     SerialMessageFormatter messageFormatter() {
-        return new SerialMessageFormatter() {
-            @Override
-            public MqttMessage getMessageForDeviceOutput(DeviceOutput deviceOutput) {
-                String message = deviceOutput.getLine()+" "+deviceOutput.getTimestamp();
-                return new MqttMessage(message.getBytes());
-            }
-        };
+        return new RawLineProtocolMessageFormatter();
     }
 
     @Bean
